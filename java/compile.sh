61a559eecfde64e275103e219e9758d964d40f4d
Simen Sommerfeldt <simen.sommerfeldt@gmail.com>
2014-02-04 08:18:32 +0100

    the right libraries

--- java/compile.sh
+++ java/compile.sh
@@ -1,1 +1,5 @@
-javac -cp mqtt-client-0.4.0.jar PahoMqttSubscribe.java
+javac -cp RXTXcomm.jar SerialClass.java
+javac -cp mqtt-client-0.4.0.jar:RXTXcomm.jar -sourcepath . Dispatcher.java
+jar -cvf utils.jar SerialClass.class mqtt-client-0.4.0.jar RXTXcomm.jar 
+
+
