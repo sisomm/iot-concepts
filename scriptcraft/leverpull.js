bd1ac6c124a6bdb2bcf2966c1153f1c78e68bb1d
Simen Sommerfeldt <simen.sommerfeldt@gmail.com>
2014-03-09 20:16:34 +0100

    commented code better

--- scriptcraft/leverpull.js
+++ scriptcraft/leverpull.js
@@ -3,15 +3,15 @@
 
 var mqtt = require('sc-mqtt');  
 
-var client = mqtt.client('tcp://192.168.1.36:1883'); // local host is default. Otherwise use host, user/pwd
+var client = mqtt.client('tcp://192.168.1.16:1883'); // local host is default. Otherwise use host, user/pwd
 //var client = mqtt.client(); 
 client.connect();
 
-var lastSeen=new Date(); // to remember the last seen time so that we only move the skull every second
+var lastSeen=new Date(); // to remember the last seen time so that we don't spam the Arduino
 var isPresent = false;	 // is the player close to the skull?
 
 var players=server.onlinePlayers;
-var lastLoc=  new org.bukkit.Location(players[0].world, 0, 0, 0); // TODO find out how to get an empty locaiton
+var lastLoc=  new org.bukkit.Location(players[0].world, 0, 0, 0); // Empty locaiton
 																  // Now we must have one player online
 var nearby=false;												  // is in front of skull
 
@@ -21,7 +21,7 @@ client.subscribe('/arduino/1/status');
 
 var player; // To remember who pulled the switch
 
-// Here we tell Minecraft to give us control after certain events
+//Detect if a the player pulled one of the switches in front of the skull
 events.on('player.PlayerInteractEvent', function (listener, event) { 
 	var block = event.getClickedBlock(); // Which block?
 	var type = block.getType();			 // What type is it?
@@ -35,11 +35,8 @@ events.on('player.PlayerInteractEvent', function (listener, event) {
 		// Is the lever up or down?
 		var state=(block.data==4)?"1":"0";
 
-		// Only publish if it is one of the right blocks
+		// Only publish if it is one of the right blocks. You must change to your position
 		if(locString=="-249,71,210" || locString=="-251,71,210"){
-
-			//Left or right?
-			//var which=(loc.x==-249)?"0":"1";
 
 			var statusTopic='/minecraft/world/lever/'+locString+'/status';
 			var payload=state;
@@ -63,11 +60,10 @@ events.on('player.PlayerMoveEvent', function (listener, event) {
 	var fromY=70.5;   // Actually 71, but we want the skull to look straight forward
 	var fromZ=211;
 	
-
 	var now=new Date();
-	
 	var timeDiff=(now-lastSeen)/1000;
 
+	// Need this to remember where he was. Should only happen once
 	if(lastLoc.x==0){
 		console.info('no last location');
 		lastLoc=loc;
@@ -83,18 +79,21 @@ events.on('player.PlayerMoveEvent', function (listener, event) {
 	var dZ=loc.z-fromZ;
 	var distance=Math.sqrt(dY*dY+dX*dX+dZ*dZ);
 
-	var sonarPos='250,72,211';
+	var sonarPos='250,72,211';			// Change to where you put your skull/sonar
 
+	// Find out how much he moved since the last time
 	var movedX=lastLoc.x-loc.x;8
 	var movedY=lastLoc.y-loc.y;
 	var movedZ=lastLoc.z-loc.z;
 	var moved=Math.sqrt(movedX*movedX+movedY*movedY+movedZ*movedZ);
 
-	if(moved<0.1) {	// Don't bother if it hasn't moved
+	if(moved<0.1) {	// Don't bother if the player hasn't moved
 		return;
 	}
 	
-	//The sonar only works in front of the skull and at distance shorter than 20
+	// The sonar only works in front of the skull and at distance shorter than 20
+	// I have disabled the sonar since I currently don't use it and it floods the messaqe queue
+	
 	//if (distance<21 && loc.z<fromZ) client.publish('/minecraft/world/sonar/'+sonarPos,'Ping: '+Math.floor(distance),2,false);	
 
 	//Find out if the skull has company 
@@ -125,6 +124,7 @@ events.on('player.PlayerMoveEvent', function (listener, event) {
 
 })
 
+//This event handler reports if the player breaks a block. The position is not correct.
 events.on('block.BlockBreakEvent', function (listener, event){
 		var loc=event.player.location;
 		var locString=loc.x+','+loc.y+','+loc.z;
