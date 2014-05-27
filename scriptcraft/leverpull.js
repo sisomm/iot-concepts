c3bbf33679e8d6e2b0904814edcb002ba4e8ad6c
Simen Sommerfeldt <simen.sommerfeldt@gmail.com>
2014-05-27 18:12:28 +0200

    upgradet to last scriptcraft

--- scriptcraft/leverpull.js
+++ scriptcraft/leverpull.js
@@ -4,8 +4,10 @@
 var mqtt = require('sc-mqtt');  
 
 var client = mqtt.client('tcp://192.168.1.16:1883'); // local host is default. Otherwise use host, user/pwd
-//var client = mqtt.client(); 
+
 client.connect();
+
+client.publish('/minecraft/status','Reset',0,false);
 
 var lastSeen=new Date(); // to remember the last seen time so that we don't spam the Arduino
 
@@ -14,44 +16,20 @@ var lastLoc=  new org.bukkit.Location(players[0].world, 0, 0, 0); // Empty locai
 																  // Now we must have one player online
 var nearby=false;												  // The player isin front of the skull
 
-
 //Subscribe to changes in the state of the Arduino
 client.subscribe('/arduino/1/status');
 
 var player; // To remember who pulled the switch
 
-// Detect if a the player pulled one of the switches in front of the skull
-events.on('player.PlayerInteractEvent', function (listener, event) { 
-	var block = event.getClickedBlock(); // Which block?
-	var type = block.getType();			 // What type is it?
- 	player = event.player; 				 // Note who shall be alerted
-
-	if(type==org.bukkit.Material.LEVER) {
-		var loc = block.location;	
-		
+//This event handler reports if the player breaks a block. The position is not correct.
+events.on('block.BlockBreakEvent', function (event){
+		var loc=event.player.location;
 		var locString=loc.x+','+loc.y+','+loc.z;
-		
-		// Is the lever up or down?
-		var state=(block.data==4)?"1":"0";
-
-		// Only publish if it is one of the right blocks. You must change to your position
-		if(locString=="-249,71,210" || locString=="-251,71,210"){
-
-			var statusTopic='/minecraft/world/lever/'+locString+'/status';
-			var payload=state;
-
-			client.publish(statusTopic,  // which topic
-			payload,					 // the status
-			0,						  	 // QoS 
-			false); 				     // broker should retain message
-		}
-
-	}
-})
-
+		client.publish('/minecraft/world/block/'+locString+'/status','BROKEN',0,false);
+});
 
 //Make skull follow the player if closer than 10 blocks
-events.on('player.PlayerMoveEvent', function (listener, event) { 
+events.on('player.PlayerMoveEvent', function (event) { 
 	var loc=event.player.location;
 
 	// The position of the skull & The origin for the face tracker 	
@@ -93,7 +71,6 @@ events.on('player.PlayerMoveEvent', function (listener, event) {
 	
 	// The sonar only works in front of the skull and at distance shorter than 20
 	// I have disabled the sonar since I currently don't use it and it floods the messaqe queue
-	
 	//if (distance<21 && loc.z<fromZ) client.publish('/minecraft/world/sonar/'+sonarPos,'Ping: '+Math.floor(distance),2,false);	
 
 	//Find out if the skull has company 
@@ -124,12 +101,36 @@ events.on('player.PlayerMoveEvent', function (listener, event) {
 
 })
 
-//This event handler reports if the player breaks a block. The position is not correct.
-events.on('block.BlockBreakEvent', function (listener, event){
-		var loc=event.player.location;
+
+
+// Detect if a the player pulled one of the switches in front of the skull
+events.on('player.PlayerInteractEvent', function (event) { 
+	var block = event.getClickedBlock(); // Which block?
+	var type = block.getType();			 // What type is it?
+ 	player = event.player; 				 // Note who shall be alerted
+
+	if(type==org.bukkit.Material.LEVER) {
+		var loc = block.location;	
+		
 		var locString=loc.x+','+loc.y+','+loc.z;
-		client.publish('/minecraft/world/block/'+locString+'/status','BROKEN',0,false);
-});
+		
+		// Is the lever up or down?
+		var state=(block.data==4)?"1":"0";
+
+		// Only publish if it is one of the right blocks. You must change to your position
+		if(locString=="-249,71,210" || locString=="-251,71,210"){
+
+			var statusTopic='/minecraft/world/lever/'+locString+'/status';
+			var payload=state;
+
+			client.publish(statusTopic,  // which topic
+			payload,					 // the status
+			0,						  	 // QoS 
+			false); 				     // broker should retain message
+		}
+
+	}
+})
 
 
 //Handle messages
@@ -140,3 +141,4 @@ client.onMessageArrived(function(topic, message){
 
 });
 
+		
