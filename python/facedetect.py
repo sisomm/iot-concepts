2aaefcce54c5ac4668b043cb24cf862bc4dbc6a2
paitrollet <simen.sommerfeldt@gmail.com>
2016-02-13 12:33:20 +0100

    first version

---
+++ python/facedetect.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+"""
+This program is demonstration for face and object detection using haar-like features.
+The program finds faces in a camera image or video stream and displays a red box around them.
+
+Original C implementation by:  ?
+Python implementation by: Roman Stanchak, James Bowman
+"""
+import os,sys
+import cv2.cv as cv
+from optparse import OptionParser
+import paho.mqtt.client as paho
+
+# Parameters for haar detection
+# From the API:
+# The default parameters (scale_factor=2, min_neighbors=3, flags=0) are tuned
+# for accurate yet slow object detection. For a faster operation on real video
+# images the settings are:
+# scale_factor=1.2, min_neighbors=2, flags=CV_HAAR_DO_CANNY_PRUNING,
+# min_size=<minimum possible face size
+
+min_size = (20, 20)
+image_scale = 2
+haar_scale = 1.2
+min_neighbors = 2
+haar_flags = 0
+
+def detect_and_draw(img, cascade):
+    # allocate temporary images
+    gray = cv.CreateImage((img.width,img.height), 8, 1)
+    small_img = cv.CreateImage((cv.Round(img.width / image_scale),
+                   cv.Round (img.height / image_scale)), 8, 1)
+
+    # convert color input image to grayscale
+    cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
+
+    # scale input image for faster processing
+    cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
+
+    cv.EqualizeHist(small_img, small_img)
+
+    if(cascade):
+        t = cv.GetTickCount()
+        faces = cv.HaarDetectObjects(small_img, cascade, cv.CreateMemStorage(0),
+                                     haar_scale, min_neighbors, haar_flags, min_size)
+        t = cv.GetTickCount() - t
+        print "detection time = %gms" % (t/(cv.GetTickFrequency()*1000.))
+        if faces:
+            facenum=0
+            for ((x, y, w, h), n) in faces:
+                # the input to cv.HaarDetectObjects was resized, so scale the
+                # bounding box of each face and convert it to two CvPoints
+                pt1 = (int(x * image_scale), int(y * image_scale))
+                pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
+                cv.Rectangle(img, pt1, pt2, cv.RGB(255, 0, 0), 3, 8, 0)
+
+		#code copied from https://github.com/mitchtech/py_servo_facetracker/blob/master/facetracker_servo_gpio.py
+
+                x1 = pt1[0]
+                x2 = pt2[0]
+                y1 = pt1[1]
+                y2 = pt2[1]
+                midFaceX = x1+((x2-x1)/2)
+                midFaceY = y1+((y2-y1)/2)
+                facenum=facenum+1;
+                client.publish(topic+str(facenum)+"/x",str(midFaceX),0)
+                client.publish(topic+str(facenum)+"/y",str(midFaceY),0)
+
+		print topic+str(facenum)+"/x",topic+str(facenum)+"/y"
+
+    cv.ShowImage("result", img)
+
+if __name__ == '__main__':
+
+    parser = OptionParser(usage = "usage: %prog [options] [filename|camera_index]")
+    parser.add_option("-c", "--cascade", action="store", dest="cascade", type="str", help="Haar cascade file, default %default", default = "haarcascade_frontalface_alt.xml")
+    parser.add_option("-s", "--server", action="store", dest="server", type="str", help="MQTT server %default", default = "127.0.0.1")
+    (options, args) = parser.parse_args()
+
+    print("Facedect: Connecting")
+    mypid = os.getpid()
+    client = paho.Client("facedect_"+str(mypid))
+    client.connect(options.server)
+    topic="/raspberry/1/face/"
+
+    cascade = cv.Load(options.cascade)
+
+
+    if len(args) != 1:
+        parser.print_help()
+        sys.exit(1)
+
+    input_name = args[0]
+    if input_name.isdigit():
+        capture = cv.CreateCameraCapture(int(input_name))
+    else:
+        capture = None
+
+    cv.NamedWindow("result", 1)
+
+    if capture:
+        frame_copy = None
+        while True:
+            frame = cv.QueryFrame(capture)
+            if not frame:
+                cv.WaitKey(0)
+                break
+            if not frame_copy:
+                frame_copy = cv.CreateImage((frame.width,frame.height),
+                                            cv.IPL_DEPTH_8U, frame.nChannels)
+            if frame.origin == cv.IPL_ORIGIN_TL:
+                cv.Copy(frame, frame_copy)
+            else:
+                cv.Flip(frame, frame_copy, 0)
+
+            detect_and_draw(frame_copy, cascade)
+
+            if cv.WaitKey(10) >= 0:
+                break
+    else:
+        image = cv.LoadImage(input_name, 1)
+        detect_and_draw(image, cascade)
+        cv.WaitKey(0)
+
+    cv.DestroyWindow("result")
