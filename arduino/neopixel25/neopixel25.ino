d40e823dd1383e5ade9bd27ff5f71cde7087ec06
Simen <simen.sommerfeldt@gmail.com>
2016-12-27 23:08:58 +0100

    tamed scripts and used consts for timing, tidied comments

--- arduino/neopixel25/neopixel25.ino
+++ arduino/neopixel25/neopixel25.ino
@@ -7,6 +7,8 @@
 #define PIN 2
 #define WAIT 40
 #define RAINBOW 10
+#define CHASE 70
+#define CHASE_STEP 5
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -42,14 +44,14 @@ void loop() {
   colorWipe(strip.Color(0, 0, 255), WAIT); // Blue
 //colorWipe(strip.Color(0, 0, 0, 255), WAIT); // White RGBW
   // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), WAIT*2); // White
-  theaterChase(strip.Color(127, 0, 0), WAIT*2); // Red
-  theaterChase(strip.Color(0, 127, 0), WAIT*2); // Green
-  theaterChase(strip.Color(0, 0, 127), WAIT*2); // Blue
+//  theaterChase(strip.Color(127, 127, 127), CHASE); // White
+  theaterChase(strip.Color(127, 0, 0), CHASE); // Red
+  theaterChase(strip.Color(0, 127, 0), CHASE); // Green
+  theaterChase(strip.Color(0, 0, 127), CHASE); // Blue
 
   rainbow(RAINBOW); //20 
   rainbowCycle(RAINBOW); //20 
-  theaterChaseRainbow(RAINBOW*2);
+//  theaterChaseRainbow(CHASE);
 }
 
 // Fill the dots one after the other with a color
@@ -89,16 +91,16 @@ void rainbowCycle(uint8_t wait) {
 //Theatre-style crawling lights.
 void theaterChase(uint32_t c, uint8_t wait) {
   for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
+    for (int q=0; q < CHASE_STEP; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+CHASE_STEP) {
+        strip.setPixelColor(i+q, c);    //turn every CHASE_STEP pixel on
       }
       strip.show();
 
       delay(wait);
 
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      for (uint16_t i=0; i < strip.numPixels(); i=i+CHASE_STEP) {
+        strip.setPixelColor(i+q, 0);        //turn every CHASE_STEP pixel off
       }
     }
   }
@@ -107,16 +109,16 @@ void theaterChase(uint32_t c, uint8_t wait) {
 //Theatre-style crawling lights with rainbow effect
 void theaterChaseRainbow(uint8_t wait) {
   for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+    for (int q=0; q < CHASE_STEP; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+CHASE_STEP) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every CHASE_STEP pixel on
       }
       strip.show();
 
       delay(wait);
 
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      for (uint16_t i=0; i < strip.numPixels(); i=i+CHASE_STEP) {
+        strip.setPixelColor(i+q, 0);        //turn every CHASE_STEP pixel off
       }
     }
   }
