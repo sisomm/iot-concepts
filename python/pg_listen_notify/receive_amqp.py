abfc9f47d0f733d4d0b49dd33cd0cefc16658c39
Simen Sommerfeldt <simen.sommerfeldt@gmail.com>
2024-07-30 09:07:41 +0200

    first amqp

---
+++ python/pg_listen_notify/receive_amqp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import pika, sys, os
+
+def main():
+    credentials=pika.PlainCredentials(os.getenv('USERNAME'),os.getenv('PASSWORD'))
+    connection=pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
+
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
