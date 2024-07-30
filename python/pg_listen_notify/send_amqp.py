2b1a983a2773aa8314d9c28502642316662bb44f
Simen Sommerfeldt <simen.sommerfeldt@gmail.com>
2024-07-30 09:10:12 +0200

    second

--- python/pg_listen_notify/send_amqp.py
+++ python/pg_listen_notify/send_amqp.py
@@ -2,7 +2,6 @@ import pika
 import os
 
 credentials=pika.PlainCredentials(os.getenv('USERNAME'),os.getenv('PASSWORD'))
-#credentials=pika.PlainCredentials('pai','dunder3000')
 connection=pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
 channel=connection.channel() 
 channel.queue_declare(queue='hello')
